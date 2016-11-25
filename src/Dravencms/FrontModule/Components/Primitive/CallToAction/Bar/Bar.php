552d265eb16ecfe2c7d1bed7355777a222f44061
Adam Schubert <adam.schubert@sg1-game.net>
2016-11-25 03:58:56 +0100

    Namespace fix

--- src/Dravencms/FrontModule/Components/Primitive/CallToAction/Bar/Bar.php
+++ src/Dravencms/FrontModule/Components/Primitive/CallToAction/Bar/Bar.php
@@ -2,7 +2,7 @@
 
 namespace Dravencms\FrontModule\Components\Primitive\CallToAction\Bar;
 
-use Dravencms\Components\BaseControl;
+use Dravencms\Components\BaseControl\BaseControl;
 use Salamek\Cms\ICmsActionOption;
 
 class Bar extends BaseControl
